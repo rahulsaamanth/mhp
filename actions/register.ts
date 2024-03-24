a46fe07aff89775c737197e6fdee1694e504c42d
rahulsaamanth <rahulsaamanth@yahoo.com>
2024-03-24 19:13:18 +0530

    updated

---
+++ actions/register.ts
@@ -0,0 +1,37 @@
+"use server"
+
+import * as z from "zod"
+import bcrypt from "bcryptjs"
+
+import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/utils/user"
+import { generateVerificationToken } from "@/lib/tokens"
+import { sendVerificationEmail } from "@/lib/mail"
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values)
+
+  if (!validatedFields.success) return { error: "Invalid fields!" }
+
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) return { error: "Email already in use!" }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  const verifcationToken = await generateVerificationToken(email)
+
+  await sendVerificationEmail(verifcationToken.email, verifcationToken.token)
+
+  return { success: "Confirmation email sent!" }
+}
---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import React, { useState } from "react"
+import * as z from "zod"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { NewPasswordSchema } from "@/schemas"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+import { CardWrapper } from "./card-wrapper"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { FormError } from "../form-error"
+import { FormSuccess } from "../form-success"
+import { newPassword } from "@/actions/new-password"
+import { useSearchParams } from "next/navigation"
+
+export const NewPasswordForm = () => {
+  const searchParams = useSearchParams()
+  const token = searchParams.get("token")
+
+  const [error, setError] = useState<string | undefined>("")
+  const [success, setSuccess] = useState<string | undefined>("")
+  const [isPending, startTransition] = React.useTransition()
+
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    setError("")
+    setSuccess("")
+
+    startTransition(() => {
+      newPassword(values, token).then((data) => {
+        setError(data?.error)
+        setSuccess(data?.success)
+      })
+    })
+  }
+
+  return (
+    <CardWrapper
+      headerLabel="Enter a new Password"
+      backButtonLabel="Back to Login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="********"
+                      type="password"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Reset Pasword
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  )
+}
