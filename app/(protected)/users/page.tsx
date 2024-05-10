5d539006861bf00149dc690757b8a7df4429b214
rahulsaamanth <rahulsaamanth@yahoo.com>
2024-05-10 14:40:14 +0530

    update- May 10 2024 2:40 PM

--- app/(protected)/users/page.tsx
+++ app/(protected)/users/page.tsx
@@ -1,15 +1,24 @@
 import { getUsers } from "@/actions/users";
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 // import Users from "@/components/users"
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 const UsersPage = async () => {
   const users = await getUsers();
 
   return (
-    // TODO: to make a UsersPage
     <>
-      <p></p>
+      <ul>
+        {users?.data?.map((user) => (
+          <li key={user.id}>
+            <Button variant="link">
+              <Link href={`/users/${user.id}`}>{user.id}</Link>
+            </Button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
