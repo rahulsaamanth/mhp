e7bb6e1ab809e08e7ad3c4e4492276661bb79dbd
rahulsaamanth <rahulsaamanth@yahoo.com>
2024-12-10 23:53:33 +0530

    working on dashboard

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,21 +1,45 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { db } from "@/db/db"
+import { order, user } from "@/db/schema"
+import { count, eq, sql, sum } from "drizzle-orm"
 import { Boxes, DollarSign, ShoppingCart, Users } from "lucide-react"
 
+async function getSalesData() {
+  const [data] = await db
+    .select({
+      salesCount: count(),
+      totalAmount: sum(order.totalAmountPaid),
+    })
+    .from(order)
+
+  return {
+    amount: data?.totalAmount,
+    numberOfSales: data?.salesCount,
+  }
+}
+
+async function getUserData() {
+  const [data] = await db
+    .select({
+      usersWithOrders: sql<number>`count(distinct case when ${order.id} is not null then ${user.id} end)`,
+      totalUsers: count(),
+    })
+    .from(user)
+    .where(eq(user.role, "USER"))
+    .leftJoin(order, eq(user.id, order.userId))
+
+  return {
+    activeUsers: data?.usersWithOrders,
+    totalUsers: data?.totalUsers,
+  }
+}
+
 const DashboardPage = async () => {
+  const salesData = await getSalesData()
+  const userData = await getUserData()
+  console.log(salesData, userData)
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-          <DollarSign className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">$45,231.89</div>
-          <p className="text-xs text-muted-foreground">
-            +20.1% from last month
-          </p>
-        </CardContent>
-      </Card>
+    <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Total Customers</CardTitle>
