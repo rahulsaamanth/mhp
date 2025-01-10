c54a92ba18a4bb77de7211f11b6d7f9c5e96ef63
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-01-10 15:20:16 +0530

    Apply codemod changes

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/db/db"
+import { product } from "@/db/schema"
+import { eq } from "drizzle-orm"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    // const { id } = await params
+    if (!params.id)
+      return NextResponse.json(
+        {
+          error: "Product ID is required",
+        },
+        { status: 400 }
+      )
+    const _product = await db.query.product.findFirst({
+      where: eq(product.id, params.id),
+      with: {
+        category: true,
+        manufacturer: true,
+        variants: true,
+      },
+    })
+    if (!_product)
+      return NextResponse.json(
+        {
+          error: "Product not found",
+        },
+        { status: 404 }
+      )
+    return NextResponse.json(_product)
+  } catch (error) {
+    console.error("Error fetching product:", error)
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    )
+  }
+}
